7c1e1ba05f9b2f9ac6836c32f70c396816de0ae8
LearnTB_Still <2622453437@qq.com>
2026-09-03 21:46:00 +0800

    正确版

--- LC/LC20.cpp
+++ LC/LC20.cpp
@@ -1,6 +1,3 @@
-#include<stack>
-#include<string>
-using namespace std;
 class Solution {
 public:
     bool isValid(string s) {
@@ -8,11 +5,12 @@ public:
         for(auto p=0;p<s.size();p++)
         {
             if(s[p]=='('||s[p]=='['||s[p]=='{') {kuohao.push(s[p]);continue;}//应当压入s[p]而非整个字符串s
+            if(kuohao.empty()){return false;}//判空防止pop报错
             if(s[p]==')'&&kuohao.top()!='(') {return false;}
             if(s[p]=='}'&&kuohao.top()!='{') {return false;}
             if(s[p]==']'&&kuohao.top()!='[') {return false;}
             kuohao.pop();
         }
-        return true;
+        return kuohao.empty();//要判断还有没有剩余的左括号
     }
 };
