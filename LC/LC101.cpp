db369cf264dfffe64c6c4a42409529c0850f8265
LearnTB_Still <2622453437@qq.com>
2026-09-17 20:30:52 +0800

    对称二叉树

---
+++ LC/LC101.cpp
@@ -0,0 +1,23 @@
+#include<string>
+using namespace std;
+struct TreeNode {
+   int val;
+   TreeNode *left;
+   TreeNode *right;
+};
+bool isSymmetric(TreeNode* root) {
+        if(root==NULL){return false;}
+        else{
+           return check(root->left,root->right);
+        }
+    }
+    bool check(TreeNode* p,TreeNode* q)
+    {
+        if(p==NULL&&q==NULL){return true;}
+        else if(p==NULL||q==NULL){
+            return false;}
+        else if(p->val!=q->val){
+            return false;
+        }
+        return check(p->left,q->right)&&check(p->right,q->left);
+    }
